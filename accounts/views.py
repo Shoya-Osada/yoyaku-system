8c80c36d89f9cb3212fca350635a15d5393b07b6
Shoya-Osada <s.oauspicious@gmail.com>
2022-03-15 17:23:43 +0900

    commit

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from allauth.account import views 
 
 
+
 class ProfileView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         user_data = CustomUser.objects.get(id=request.user.id)
